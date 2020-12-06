f8b96603b05cd38393816f63bc02c01bf42e0b10
DanaSong <dahae0624@naver.com>
2020-12-06 14:33:49 +0900

    1205

--- js/resume.js
+++ js/resume.js
@@ -1,71 +1,29 @@
 (function($) {
 
-    $(".section").on("mousewheel", function (e, wh) {
-        var index = $(this).index()
-        //마우스 휠을 올렸을때
-        if (wh > 0) {
-            var prev = $(this).prev().offset().top;
-            $('.depth1 li').eq(index - 1).addClass('on')
-            $('.depth1 li').eq(index - 1).siblings().removeClass('on')
-            $("html,body").stop().animate({
-                scrollTop: prev
-            }, 1000, "linear");
+    $(window).on('load', function(){
+        $('.introAni').delay(2000).fadeOut(500)
+    })
 
-        //마우스 휠을 내렸을때	 
-        } else if (wh < 0) {
-            var next = $(this).next().offset().top;
-            $('.depth1 li').eq(index + 1).addClass('on')
-            $('.depth1 li').eq(index + 1).siblings().removeClass('on')
-            $("html,body").stop().animate({
-                scrollTop: next
-            }, 1000, "linear");
-        }
+    var myFullpage = new fullpage('#fullpage', {
+        anchors: ['home', 'profile', 'publishing', 'portfolio', 'contact'],
+        lazyLoad: true
     });
 
 
     // 스크롤 탑일때와 아닐때 -- 헤더 상단 / gotop / 스킬 적용
     var sct = 0;
-    $(window).scroll(function () {
+    $(window).on('hashchange', function () {
 
-        sct = $(this).scrollTop();
-        var winHeight = $(this).height();
-
-
-        // // 스크롤 시 헤더 색상
-        if (sct >= winHeight) {
-            $(".header-outer").css({
-                color: '#61BC8F'
-            });
+        var hashtagname = location.hash;
+        if (hashtagname == "#profile" || hashtagname == "#publishing" || hashtagname == "#portfolio" ) {
+            $('.header-outer').addClass('on')
         } else {
-            $(".header-outer").css({
-                color: '#fff'
-            });
+            $('.header-outer').removeClass('on')
         }
-
-        // 해당화면 온오프
-        if (sct >= $('#section1').offset().top) {
-            $('.home').stop().fadeIn(600)
-        } else {
-            $('.home').hide()
-        }
-        if (sct >= $('#section2').offset().top) {
-            $('.profile').stop().fadeIn(300)
-        } else {
-            $('.profile').hide()
-        }
-        if (sct >= $('#section3').offset().top) {
-            $('.publishing').stop().fadeIn(300)
-        } else {
-            $('.publishing').hide()
-        }
-        if (sct >= $('#section4').offset().top) {
-            $('.portfolio').stop().fadeIn(300)
-        } else {
-            $('.portfolio').hide()
-        }
+    
 
 
-        // 포트폴리오 순차적 띄우기
+        /* // 포트폴리오 순차적 띄우기
         var portcount = $('.gallery > li').length
         if (sct >= $('.portfolio').offset().top) {
             for (var i = 0; i < portcount; i++) {
@@ -73,7 +31,7 @@
                     animationDelay: (i + 1) * 0.15 + 's'
                 })
             }    
-        }
+        } */
     
     });
 
@@ -81,12 +39,13 @@
     $('.publishing').slick({
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 2000,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
+        pauseOnHover: false,
         prevArrow: false,
-        nextArrow: '<button class="nextArrow marrow"><i class = "fas fa-angle-right"></i></button>',
+        nextArrow: false,
     })
 
 
