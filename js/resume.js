1b486f7412d9bd4dea25431bfd9722a5a8e7d83f
DanaSong <dahae0624@naver.com>
2020-12-07 10:45:42 +0900

    1207

--- js/resume.js
+++ js/resume.js
@@ -86,4 +86,56 @@
          return false;
      })
 
+
+
+     
+    //포트폴리오 갤러리 클릭 이벤트 팝업
+    var href, src, alt, lieq
+
+    $('.gallery > li > a').on('click', function(e){
+        e.preventDefault();
+        lieq = $(this).parent().index()
+        console.log(lieq)
+        $('.galleryPopup').addClass('on')
+        href = $(this).attr('href')
+        src = $(this).find('img').attr('src')
+        alt = $(this).find('img').attr('alt')
+        $('.popupList > div > a').attr('href', href)
+        $('.popupList > div > a > img').attr('src', src)
+        $('.popupList > div > a > img').attr('alt', alt)
+    })
+
+    $('.galleryPopup .close').on('click', function(){
+        $('.galleryPopup').removeClass('on')
+    })
+
+    $('.popupList').on('click', function(e){
+        e.stopPropagation();
+    })
+
+    function changeList(ind) {
+        href = $('.gallery > li').eq(ind).find('a').attr('href')
+        src = $('.gallery > li').eq(ind).find('img').attr('src')
+        alt = $('.gallery > li').eq(ind).find('img').attr('alt')
+        $('.popupList > div > a').attr('href', href)
+        $('.popupList > div > a > img').attr({'src': src, 'alt': alt}).css({opacity:'0.5'}).animate({opacity:'1'}, 500)
+    }
+
+
+    $('.popupList .prev').on('click', function(){
+        --lieq
+        if (lieq < 0) {
+            lieq = 5
+        }
+        changeList(lieq)
+    })
+
+    $('.popupList .next').on('click', function(){
+        ++lieq
+        if (lieq > 5) {
+            lieq = 0
+        }
+        changeList(lieq)
+    })
+
 })(jQuery)
